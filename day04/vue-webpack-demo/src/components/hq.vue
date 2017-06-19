c0e92c1cde3f1cddd604aafce4aa2244a0a342d8
xf <hq754730@sina.com>
2017-06-20 06:27:52 +0800

    2017-06-20 06:30

---
+++ day04/vue-webpack-demo/src/components/hq.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hq">
+    {{msg}}
+    <Hello></Hello>
+  </div>
+</template>
+<script>
+  import Hello from './Hello'
+  export default {
+    data () {
+      return {msg: 'sdfasfasfasf'}
+    },
+    components: {
+      Hello
+    }
+  }
+</script>
+<style>
+</style>

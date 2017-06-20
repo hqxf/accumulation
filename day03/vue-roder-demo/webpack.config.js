9e414282bba4f14e91bcb7a869380884f713dd62
hq <754730157@qq.com>
2017-06-20 08:15:31 +0800

    2016-06-20 08:15

---
+++ day03/vue-roder-demo/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports={
+	entry:"./main.js",
+	output:{
+		path:__dirname,
+		filename:"build.js"
+	},
+	module:{
+		loads:{
+			{test:/\.vue$/,loader:'vue'},
+			{test:/\.js$/,loader:"babel",exclude:/node_module/}
+		},
+		babel:{
+			presets:['es2015'],
+			plugins:
+		}
+	}
+}
